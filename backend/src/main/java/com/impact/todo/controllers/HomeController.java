637f646dfeff0e68a6aabf102a303ef2866122ec
MoepyaCode <kagisomoepya19@gmail.com>
2024-11-10 16:21:21 +0200

    ✅ Added browser configuration support

---
+++ backend/src/main/java/com/impact/todo/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.impact.todo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the Todo Application!";
+    }
+}
